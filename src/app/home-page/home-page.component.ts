99a3ec5bfd8bd97dbda4c462ccdfd5b02dbb68ca
Sailendra Darbha <sailendra.darbha@gmail.com>
2023-09-28 15:52:53 +0530

    desc. short added, color changes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  description:string = "Hello there! \n My Name is Dr. Sailendra Darbha, I'm a dentist by education developer by profession, building interfaces is my passion which led me on a wild self-taught coding journey."
+
 }
