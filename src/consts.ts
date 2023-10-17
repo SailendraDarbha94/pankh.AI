89b4a425527bcaf7785856067ef010143b2d42be
Sailendra Darbha <sailendra.darbha@gmail.com>
2023-10-18 02:39:36 +0530

    portfolio ready for basic

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,14 @@
 
 export const SITE_TITLE = 'Sailendra Darbha';
 export const SITE_DESCRIPTION = 'Welcome to my personal Portfolio!';
+
+export const IDS = "I learned industry-standard theory and methods for developing successful user interfaces. You now have fluency with the user research, prototyping and evaluation techniques necessary for creating intuitive interfaces that facilitate good user experiences. I demonstrated this fluency through an in-depth capstone project. Concepts and techniques covered include structured approaches for understanding a user base and their needs (e.g. contextual inquiry and design psychology), prototyping and design methods (e.g. low- fidelity and paper prototyping), and techniques for evaluating our design choices (e.g. heuristic evaluation, user studies)."
+
+export const PDS = "The University of Michigan offers a Python Data Structures course as part of its online learning curriculum. This course is designed to teach students fundamental concepts related to data structures in Python. It covers essential topics such as lists, dictionaries, and tuples, along with an emphasis on organizing, retrieving, and manipulating data efficiently. The course provides hands-on coding exercises to reinforce learning, making it suitable for beginners and those looking to strengthen their Python skills. It is delivered through Coursera, offering flexibility for self-paced learning. Upon completion, students gain valuable insights into data management and manipulation in Python, which are essential skills for data science and programming. This course is a solid foundation for those pursuing more advanced programming and data science courses."
+
+export interface Certificate {
+    name: string;
+    date: string;
+    uni: string;
+    desc: string;
+}
