affa5ed572d463b1c73889e7fc0ac80c64c792a2
Ashutosh-8055-Tiwari <ashu.8055.tiwari@gmail.com>
2024-04-27 17:01:40 +0530

    layout updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
-import type { Metadata } from "next";
-import { Roboto, Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
+import { useRouter } from 'next/navigation';
+import type { Metadata } from 'next';
+import { Roboto, Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 const roboto = Roboto({
-  weight: ["400", "700"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  display: "swap",
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
 });
+
 export const metadata: Metadata = {
-  title: "Pankh.AI",
-  description: "A platform for private llm models trained on your custom data",
+  title: 'Pankh.AI',
+  description: 'A platform for private llm models trained on your custom data',
 };
 
 export default function RootLayout({
@@ -20,10 +22,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
+
+  const hideNavbar = router.pathname.startsWith('/dashboard/default') || router.pathname.startsWith('/dashboard/admin');
+
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <Navbar className="dark hidden md:block"/>
+        {!hideNavbar && <Navbar className="dark hidden md:block" />}
         <div className="">{children}</div>
       </body>
     </html>
