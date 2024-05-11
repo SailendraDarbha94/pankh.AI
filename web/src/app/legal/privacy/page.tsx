898a59ccb9802f553b44888523a983abc2d61706
Ashutosh Tiwari <140781443+Ashutosh-8055-Tiwari@users.noreply.github.com>
2024-05-11 01:23:45 -0700

    added terms and privacy

--- web/src/app/legal/privacy/page.tsx
+++ web/src/app/legal/privacy/page.tsx
@@ -1,8 +1,125 @@
 "use client"
 
+
 const Page = () => {
     return (
-        <div className="w-full min-h-screen">Page</div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+      <div>
+      <section className="bg-white dark:bg-gray-900 mb-20 font-raleway rounded-xl border-b-4 ">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <h2 className="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            Privacy Policy:
+            </h2>
+            <div className="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+              <div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Information Collection:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  Pankh.Ai collects and processes personal data for the purpose of providing and improving our services.
+                   This may include information such as name, email address, and usage data.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Use of Information:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  Personal data collected by Pankh.Ai may be used for purposes including but not limited to:
+                  </p>
+                
+                  <li className="text-gray-500 dark:text-gray-400">
+                  Providing, maintaining, and improving our services
+                  </li>
+                  <li className="text-gray-500 dark:text-gray-400">
+                  Communicating with users
+                  </li>
+                  <li className="text-gray-500 dark:text-gray-400">
+                  Analyzing usage trends and preferences
+                  </li>
+                  <li className="text-gray-500 dark:text-gray-400">
+                  Customizing content and advertising
+                  </li>
+                  <li className="text-gray-500 dark:text-gray-400">
+                  Complying with legal obligations
+                  </li>
+              
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Data Sharing:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  Pankh.Ai may share personal data with third-party service providers for the purpose 
+                  of providing and improving our services. We do not sell or rent personal data to
+                   third parties for their own marketing purposes.
+                  </p>
+                  
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Data Security:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                   Pankh.Ai takes reasonable measures to protect personal data against unauthorized access,
+                   alteration, disclosure, or destruction. However, no method of transmission over the 
+                   internet or electronic storage is 100% secure.
+                  </p>
+                 
+                </div>
+              </div>
+              <div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Data Retention:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  Pankh.Ai retains personal data for as long as necessary to fulfill the purposes outlined in this 
+                  privacy policy, unless a longer retention period is required or permitted by law.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Your Rights:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  You have the right to access, update, or delete your personal data held by Pankh.Ai. 
+                  You may also have the right to restrict or object to certain processing activities.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Changes to this Privacy Policy:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  Pankh.Ai may update this privacy policy from time to time. Any changes will be posted on this page, 
+                  and you are encouraged to review this policy periodically for updates.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Contact Us:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  If you have any questions or concerns about this privacy policy or our data practices,
+                   please contact us at pank.ai@support.com.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      </main>
     )
 }
 
--- web/src/app/legal/terms/page.tsx
+++ web/src/app/legal/terms/page.tsx
@@ -1,8 +1,122 @@
 "use client"
 
+
 const Page = () => {
     return (
-        <div className="w-full min-h-screen">Page</div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 border-b-4 border-black">
+      <div>
+      <section className="bg-white dark:bg-gray-900 mb-20 font-raleway rounded-xl border-b-4">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <h2 className="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            Terms and Conditions:
+            </h2>
+            <div className="grid pt-8 text-left border-t border-gray-200 md:gap-16 dark:border-gray-700 md:grid-cols-2">
+              <div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Introduction:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                   Welcome to Pankh.Ai. These terms and conditions govern your use of our services,
+                   including the deployment of multiple Language Models (LLMs) within a limited storage space of 1, 
+                   utilizing custom training and fine-tuning capabilities.
+                   By accessing or using our services, you agree to be bound by these terms and conditions.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Service Description:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                   Pankh.Ai provides a service that enables users to deploy multiple Language Models (LLMs)
+                   within a limited storage space of 1, utilizing proprietary custom training 
+                   and fine-tuning techniques. Users are granted a limited, non-exclusive, 
+                   non-transferable license to use the service for its intended purpose.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                    Intellectual Property Rights:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    All intellectual property rights associated with the service,
+                    including custom training methodologies, fine-tuning techniques,
+                    and software algorithms, are owned by Pankh.Ai.
+                    Users are prohibited from reproducing, modifying, distributing, 
+                    or creating derivative works based on the service without prior written consent.
+                  </p>
+                  
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  User Responsibilities:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  Users agree to use the service in compliance with all applicable laws and regulations.
+                   You are responsible for maintaining the security of your account and any activities 
+                   that occur under your account. You must not attempt to interfere with the proper
+                    functioning of the service or access it using unauthorized methods.
+                  </p>
+                 
+                </div>
+              </div>
+              <div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Limitation of Liability:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  Pankh.Ai shall not be liable for any direct, indirect, incidental, special, 
+                  consequential, or exemplary damages, including but not limited to, damages 
+                  for loss of profits, goodwill, use, data, or other intangible losses resulting 
+                  from the use or inability to use the service.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Indemnification:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                   You agree to indemnify and hold harmless Pankh.Ai, its affiliates, officers, directors,
+                   employees, agents, and licensors from and against any and all claims, liabilities,
+                   damages, losses, costs, expenses, or fees arising out of or relating to your
+                   use of the service.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Termination:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  Pankh.Ai reserves the right to suspend or terminate access to the service at any time,
+                   with or without cause, and with or without notice. Upon termination, your right to use
+                    the service will cease immediately.
+                  </p>
+                </div>
+                <div className="mb-10">
+                  <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 dark:text-white">
+                    
+                  Governing Law:
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                  These terms and conditions shall be governed by and construed in accordance with 
+                  the laws of India, and you submit to the exclusive jurisdiction of the courts located
+                   in India for the resolution of any disputes.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      </main>
     )
 }
 
